f44c4dfca20e77127b465ab81868bad37d94295a
brunosj3 <brunobruneco@live.com>
2024-07-27 16:57:26 -0300

    a

--- Java Software Design/src/Samples/Animal/AnimalTester.java
+++ Java Software Design/src/Samples/Animal/AnimalTester.java
@@ -7,14 +7,18 @@ public class AnimalTester
     public static void main(String[] args)
 
     {
-        Shark shark = new Shark();
+        /*
+         * Shark shark = new Shark();
+         * 
+         * shark.getScentificName();
+         * shark.getPopularName();
+         * shark.getAge();
+         * shark.getMass();
+         * shark.setNumverOfFins(2);
+         */
 
-        shark.getScentificName();
-        shark.getPopularName();
-        shark.getAge();
-        shark.getMass();
-        shark.setNumverOfFins(2);
-
+        Shark shark = new Shark("Galeocerdo cuvier", "tubarão tigre", 20, 250.0, 2);
+        // puitter
         System.out.println(shark.getScentificName());
         System.out.println(shark.getPopularName());
         System.out.println(shark.getAge());
